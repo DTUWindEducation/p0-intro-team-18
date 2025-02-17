3d0d7be624884d8339ca6adfe901ae6e24c4c867
Irene-GL <s250310@dtu.dk>
2025-02-17 10:32:51 +0100

    Add preclass_assignment

--- preclass_assignment/week00_prep_answers.py
+++ preclass_assignment/week00_prep_answers.py
@@ -1,107 +1,66 @@
 # Question 1
-def greet(person):
-    """
-    This Function prints to the inputted person
+def greet(name):
+    print(f"Hello, {name}!")
 
-    Args:
-        person (str): The person that you would like to say to
-    """
-    print("Hello, " + person+ "!")
-    
 greet('world')
 
-
 # Question 2
-def goldilocks(bedSize):
-    """
-    This function determines prints if a given bed size is too small, too large
-    or just right for goldilocks 
-
-    Args:
-        bedSize (int): the size of the proposed bed
-    """
-    if bedSize<140:
-        print("Too small.")
-    elif bedSize>150:
-        print("Too large.")
+def goldilocks(bed_length):
+    if bed_length < 140:
+        print("Too small!")
+    elif bed_length > 150:
+        print("Too large!")
     else:
         print("Just right. :)")
 
+## Test
 goldilocks(139)
 goldilocks(140)
-goldilocks(150)
 goldilocks(151)
-
+goldilocks(150)
 
 # Question 3
-def square_list(num_list):
-    """
-    This function takes in a list of numbers of size n and returns the square of each number
-    in a list of size n
-
-    Args:
-        num_list (list): a list of numbers
-
-    Returns:
-        list: a list of each number squared
-    """
-    sqd_list=[]
-    for number in num_list:
-        squared = number**2
-        sqd_list.append(squared)
-    return sqd_list
-    
+## Option 1
+def square_list(numbers):
+    squared_numbers = []
+    for num in numbers:
+        squared_numbers.append(num ** 2)
+    return squared_numbers
 print(square_list([1, 2, 3]))
 
+## Option 2
+def square_list(numbers):
+    return [num ** 2 for num in numbers]
 
-#Question 4
-def fibonacci_stop(max_num):
-    """
-    This function returns a list of all the numbers in the fibonacci sequence that are less than or equal to
-    the inputted max_num.
+print(square_list([1, 2, 3]))
 
-    Args:
-        max_num (int): the upper limit to the value of any element in the fibbonaci sequence
+# Question 4
+def fibonacci_stop(max_value):
+    fib_sequence = [1,1]
 
-    Returns:
-        list: an ordered list of numbers in the fibbonacci sequence that are less than or equal to max_num
-    """
-    current_val=1
-    prev_val=0
-    temp_current_val=-1
-    seq_list=[]
-    while current_val <= max_num:
-        temp_current_val=current_val
-        current_val+=prev_val
-        prev_val=temp_current_val
-        
-        seq_list.append(prev_val)
-    return seq_list
-    
+    while True:
+        next_fib = fib_sequence[-1] + fib_sequence[-2]
+        if next_fib > max_value:
+            break
+        fib_sequence.append(next_fib)
+
+    return fib_sequence
+
+##test
 print(fibonacci_stop(30))
 
-#Question 5
-def clean_pitch(x, status):
-    """
-    Compares the inputted x and status lists element-wise. If the status element is 1 then the 
-    corresponding element in the x list is checked to see if it is beween [0, 90]. If it is then
-    that x element is added to the output list if not then -999 is added to the output list.
 
-    Args:
-        x (list): list of angles
-        status (list): list of whether a measurement is valid
-        
-    Returns:
-        list: a list of cleaned angle values
-    """
-    cleaned_x=[]
-    for index in range(len(x)):
-        if (x[index]>90 or x[index]<0) and status[index]>0:
-            cleaned_x.append(-999)
-        elif status[index]<0:
-            cleaned_x.append("Invalid Status")
+# Question 5
+def clean_pitch(pitch_angles, status_signals):
+    cleaned_data = []
+    for pitch, status in zip(pitch_angles, status_signals):
+        if (pitch <0 or pitch > 90) and status > 0:
+            cleaned_data.append(-999)
         else:
-            cleaned_x.append(x[index])
-    return cleaned_x
-    
-print(clean_pitch([-1, 2, 6, 95], [1, 0, 0, 0]))
+            cleaned_data.append(pitch)
+    return cleaned_data
+
+##test 
+x = [-1, 2, 6, 95]
+status = [1, 0, 0, 0]
+print(clean_pitch(x, status))
